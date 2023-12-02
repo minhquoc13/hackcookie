ebd96e01b886ac7add50a032cfab010ae6ec585e
minhquoc13 <mq19052002@gmail.com>
2023-12-02 22:24:48 +0700

    add shopeefake

--- server/server.js
+++ server/server.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const cors = require("cors");
 const app = express();
+
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
